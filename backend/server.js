71ece3704e8a7876c1b9171a649c33fa1a35fc37
Suganya Maheswaran <43247716+Suganya1990@users.noreply.github.com>
2025-05-16 10:13:36 -0400

    finished backend sever

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import dotenv from "dotenv"
 import { connectDB } from './config/db.js';
+import productRoutes  from "./routes/product.route.js"
 
-
+dotenv.config()
 const app = express();
 
-app.get("/products", (req, res)=>{
-})
+app.use(express.json());
 
-console.log(process.env.MONGO_URI)
+app.use("/api/products", productRoutes )
+
 app.listen(5000, ()=>{
+
     connectDB();
     console.log("Server started at http://localhost:5000")
 })
